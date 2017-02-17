f28c1950b5f9911f7717e192e2b797ce1bdb8c18
cignal-udse <udse.web@gmail.com>
2017-02-18 01:36:31 +0800

    Added forgot password function

--- assets/js/websitejs/floatingMenuDir.js
+++ assets/js/websitejs/floatingMenuDir.js
@@ -1,12 +1,13 @@
 app.controller("floatingMenuCtrl", function($scope) {
     console.log('floatingMenuCtrl')
 
-    $scope.loader = { isLoadingData: true, isSigningUp: false, isLoggingIn: false, isLoggingOut: false };
+    $scope.loader = { isLoadingData: true, isSigningUp: false, isLoggingIn: false, isLoggingOut: false, isForgotPassword: false };
     $scope.activeUser = Parse.User.current();
     $scope.user = {};
     $scope.branches = [];
     $scope.newData = { };
     $scope.retypePassword = '';
+    $scope.forgotEmail = '';
 
     if ($scope.activeUser) {
         if ($scope.activeUser.get('type') != 'CUSTOMER') {
@@ -125,6 +126,34 @@ app.controller("floatingMenuCtrl", function($scope) {
                 location.reload();
             }
         });
+    };
+
+    // forgot password function
+    $scope.forgotPassword = function() {
+        if (!$scope.loader.isForgotPassword) {
+            if (!$scope.forgotEmail) {
+                notification('Email required', 'danger');
+            } else {
+                $scope.loader.isForgotPassword = true;
+                Parse.User.requestPasswordReset($scope.forgotEmail, {
+                    success: function() {
+                        notification('An email is sent to reset your password');
+                        $scope.forgotEmail = '';
+                        var modal = UIkit.modal("#forgotpass");
+                        modal.hide();
+                        $scope.loader.isForgotPassword = false;
+                        $scope.$apply();
+                    },
+                    error: function(error) {
+                        console.log(error)
+                        console.log("Error: " + error.code + " " + error.message);
+                        notification('Something is wrong', 'danger');
+                        $scope.loader.isForgotPassword = false;
+                        $scope.$apply();
+                    }
+                });
+            }
+        }
     };
 
     $scope.offcanvasLogin = function() {
